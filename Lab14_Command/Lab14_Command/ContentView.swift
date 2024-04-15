cdcedc0d2b1bb53fb683d0618986e9c70107486a
Kiril <kiril.turov.05@icloud.com>
2024-04-16 01:08:41 +0200

    Завантаження лабораторної роботи №14

---
+++ Lab14_Command/Lab14_Command/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab14_Command
+//
+//  Created by Kiril Turov on 2024-04-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab14_Command/Lab14_Command/Lab14_CommandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab14_CommandApp.swift
+//  Lab14_Command
+//
+//  Created by Kiril Turov on 2024-04-16.
+//
+
+import SwiftUI
+
+@main
+struct Lab14_CommandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
